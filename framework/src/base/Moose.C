451e774be3eb39b15282808be7d5832b0a705f16
Benjamin Spencer <benjamin.spencer@inl.gov>
2014-03-03 12:45:30 -0700

    Squash all XFEM commits that touch MOOSE

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -94,6 +94,8 @@
 #include "VectorMagnitudeAux.h"
 #include "ConstantScalarAux.h"
 #include "QuotientAux.h"
+#include "XFEMVolFracAux.h"
+#include "XFEMCutPlaneAux.h"
 #include "NormalizationAux.h"
 #include "VariableGradientComponent.h"
 #include "ParsedAux.h"
@@ -466,6 +468,8 @@ registerObjects(Factory & factory)
   registerAux(VectorMagnitudeAux);
   registerAux(ConstantScalarAux);
   registerAux(QuotientAux);
+  registerAux(XFEMVolFracAux);
+  registerAux(XFEMCutPlaneAux);
   registerAux(NormalizationAux);
   registerAux(FunctionScalarAux);
   registerAux(VariableGradientComponent);
