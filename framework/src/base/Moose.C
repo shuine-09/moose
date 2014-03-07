adfdabda38ac5c149a0a5971156781ceaad12480
Benjamin Spencer <benjamin.spencer@inl.gov>
2014-03-06 18:10:10 -0700

    Work in progress to make auxKernel to identify crack tip elements

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -96,6 +96,7 @@
 #include "QuotientAux.h"
 #include "XFEMVolFracAux.h"
 #include "XFEMCutPlaneAux.h"
+#include "XFEMMarkerAux.h"
 #include "NormalizationAux.h"
 #include "VariableGradientComponent.h"
 #include "ParsedAux.h"
@@ -470,6 +471,7 @@ registerObjects(Factory & factory)
   registerAux(QuotientAux);
   registerAux(XFEMVolFracAux);
   registerAux(XFEMCutPlaneAux);
+  registerAux(XFEMMarkerAux);
   registerAux(NormalizationAux);
   registerAux(FunctionScalarAux);
   registerAux(VariableGradientComponent);
