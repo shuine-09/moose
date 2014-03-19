1e72780e994cd8722f0ae5c5e92eb08e9925b8ab
Benjamin Spencer <benjamin.spencer@inl.gov>
2014-03-19 17:55:59 -0600

    Mark cuts by state
    Modify XFEM class to include a vector of elements marked for cutting
    based on state, and cut those elements.  Add an ElementUserObject that
    determines whether elements should be cut based on state adds elements
    to be cut to the XFEM object's vector of elements to be cut.  Still need
    to modify the UserObject to mark based on something meaningful, but
    the framework is in place now.

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -236,6 +236,7 @@
 #ifdef LIBMESH_HAVE_FPARSER
 #include "Terminator.h"
 #endif
+#include "XFEMMarkerUserObject.h"
 
 // preconditioners
 #include "PhysicsBasedPreconditioner.h"
@@ -642,6 +643,7 @@ registerObjects(Factory & factory)
 #ifdef LIBMESH_HAVE_FPARSER
   registerUserObject(Terminator);
 #endif
+  registerUserObject(XFEMMarkerUserObject);
 
   // preconditioners
   registerNamedPreconditioner(PhysicsBasedPreconditioner, "PBP");
