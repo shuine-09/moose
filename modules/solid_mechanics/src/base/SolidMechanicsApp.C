936f333d63a392e797ba74439c42b97299875c28
Benjamin Spencer <benjamin.spencer@inl.gov>
2014-03-27 18:37:32 -0600

    Add XFEMMaterialTensorMarkerUserObject.  It still needs work

--- modules/solid_mechanics/src/base/SolidMechanicsApp.C
+++ modules/solid_mechanics/src/base/SolidMechanicsApp.C
@@ -36,6 +36,7 @@
 #include "CrackFrontDefinition.h"
 #include "InteractionIntegral.h"
 #include "InteractionIntegralAuxFields.h"
+#include "XFEMMaterialTensorMarkerUserObject.h"
 #include "MaterialSymmElasticityTensorAux.h"
 #include "MaterialTensorAux.h"
 #include "MaterialTensorOnLine.h"
@@ -175,6 +176,7 @@ SolidMechanicsApp::registerObjects(Factory & factory)
   registerUserObject(MaterialTensorOnLine);
   registerUserObject(CavityPressureUserObject);
   registerUserObject(CrackFrontDefinition);
+  registerUserObject(XFEMMaterialTensorMarkerUserObject);
 }
 
 void
