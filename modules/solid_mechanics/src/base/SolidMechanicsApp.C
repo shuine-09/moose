209c9b78aec2ace971c3f3048bc8b717cf6c2226
Benjamin Spencer <benjamin.spencer@inl.gov>
2015-06-02 11:18:59 -0600

    HACK: add dummy material to get solid_mechanics state working

--- modules/solid_mechanics/src/base/SolidMechanicsApp.C
+++ modules/solid_mechanics/src/base/SolidMechanicsApp.C
@@ -73,6 +73,7 @@
 #include "RateDepSmearCrackModel.h"
 #include "RateDepSmearIsoCrackModel.h"
 #include "CutPlaneAction.h"
+#include "StatefulTest.h"
 
 template<>
 InputParameters validParams<SolidMechanicsApp>()
@@ -145,6 +146,7 @@ SolidMechanicsApp::registerObjects(Factory & factory)
   registerMaterial(TrussMaterial);
   registerMaterial(RateDepSmearCrackModel);
   registerMaterial(RateDepSmearIsoCrackModel);
+  registerMaterial(StatefulTest);
 
   registerKernel(HomogenizationKernel);
   registerKernel(SolidMechImplicitEuler);
