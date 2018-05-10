5b7a4354e7a4039759c19371e6e66b6a00a81ac4
Wen Jiang <wen.jiang@inl.gov>
2018-05-10 13:38:20 -0600

    added XFEMTwoSideDirichlet

---
+++ modules/xfem/include/constraints/XFEMTwoSideDirichlet.h
@@ -0,0 +1,53 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef XFEMTWOSIDEDIRICHLET_H
+#define XFEMTWOSIDEDIRICHLET_H
+
+// MOOSE includes
+#include "ElemElemConstraint.h"
+#include "MooseMesh.h"
+
+// Forward Declarations
+class XFEMTwoSideDirichlet;
+
+class XFEM;
+
+template <>
+InputParameters validParams<XFEMTwoSideDirichlet>();
+
+class XFEMTwoSideDirichlet : public ElemElemConstraint
+{
+public:
+  XFEMTwoSideDirichlet(const InputParameters & parameters);
+  virtual ~XFEMTwoSideDirichlet();
+
+protected:
+  virtual void reinitConstraintQuadrature(const ElementPairInfo & element_pair_info) override;
+
+  virtual Real computeQpResidual(Moose::DGResidualType type) override;
+
+  virtual Real computeQpJacobian(Moose::DGJacobianType type) override;
+
+  /// Vector normal to the internal interface
+  Point _interface_normal;
+
+  /// Stabilization parameter in Nitsche's formulation
+  Real _alpha;
+
+  /// Vector normal to the internal interface
+  Real _levelset_positive_value;
+
+  Real _levelset_negative_value;
+
+  /// Pointer to the XFEM controller object
+  std::shared_ptr<XFEM> _xfem;
+};
+
+#endif /* XFEMTWOSIDEDIRICHLET_H_ */
